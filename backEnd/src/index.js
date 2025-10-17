59443e282eedee8f490870b4511daf3b7e720723
hoangzz03 <phanhoang.12345678hp@gmail.com>
2025-10-17 13:00:30 +0700

    login fix2

--- backEnd/src/index.js
+++ backEnd/src/index.js
@@ -47,16 +47,6 @@ app.use("/api/roles", roleRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/upload", uploadRoutes);
 
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  // Express 5 syntax — dùng regex
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
